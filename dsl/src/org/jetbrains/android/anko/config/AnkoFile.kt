462f048a0674864c8e41a687a4ab1cba2d96e385
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-05-27 20:15:05 +0300

    Calculate AnkoFile names automatically

--- dsl/src/org/jetbrains/android/anko/config/AnkoFile.kt
+++ dsl/src/org/jetbrains/android/anko/config/AnkoFile.kt
@@ -16,24 +16,44 @@
 
 package org.jetbrains.android.anko.config
 
-public enum class AnkoFile(val filename: String) {
-    ASYNC : AnkoFile("Async.kt")
-    CONTEXT_UTILS : AnkoFile("ContextUtils.kt")
-    CUSTOM : AnkoFile("Custom.kt")
-    DATABASE : AnkoFile("Database.kt")
-    DIALOGS : AnkoFile("Dialogs.kt")
-    HELPERS : AnkoFile("Helpers.kt")
-    INTERFACE_WORKAROUNDS : AnkoFile("InterfaceWorkarounds.java")
-    INTERNALS : AnkoFile("Internals.kt")
-    LAYOUTS : AnkoFile("Layouts.kt")
-    LISTENERS : AnkoFile("Listeners.kt")
-    LOGGER : AnkoFile("Logger.kt")
-    OTHER : AnkoFile("Other.kt")
-    OTHER_WIDGETS : AnkoFile("OtherWidgets.kt")
-    PROPERTIES : AnkoFile("Properties.kt")
-    SERVICES : AnkoFile("Services.kt")
-    SQL_PARSER_HELPERS : AnkoFile("SqlParserHelpers.kt")
-    SQL_PARSERS : AnkoFile("SqlParsers.kt")
-    SUPPORT : AnkoFile("Support.kt")
-    VIEWS : AnkoFile("Views.kt")
+public enum class AnkoFile {
+    ASYNC
+    CONTEXT_UTILS
+    CUSTOM
+    DATABASE
+    DIALOGS
+    HELPERS
+    INTERFACE_WORKAROUNDS
+    INTERNALS
+    LAYOUTS
+    LISTENERS
+    LOGGER
+    OTHER
+    OTHER_WIDGETS
+    PROPERTIES
+    SERVICES
+    SQL_PARSER_HELPERS
+    SQL_PARSERS
+    SUPPORT
+    SUPPORT_TINTED_WIDGETS
+    VIEWS
+
+    public val filename: String = toString().toCamelCase() + ".kt"
+
+    private companion object {
+        private fun String.toCamelCase(): String {
+            val builder = StringBuilder()
+            var capitalFlag = true
+            for (c in this) {
+                when (c) {
+                    '_' -> capitalFlag = true
+                    else -> {
+                        builder.append(if (capitalFlag) Character.toUpperCase(c) else c)
+                        capitalFlag = false
+                    }
+                }
+            }
+            return builder.toString()
+        }
+    }
 }
